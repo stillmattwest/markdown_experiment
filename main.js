57c8cb46e639321d25d238d6233da01bc39fc905
stillmattwest <stillmattwest@gmail.com>
2020-04-13 22:39:48 -0700

    basic page with markdown converter

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let inBlock = false;
 function convertMarkdown(txt) {
-  const lines = txt.split("\r");
+  const lines = txt.split("\n");
   // return an array of HTML elements
   let result = [];
   let codeBlock = "";
@@ -25,7 +25,7 @@ function convertMarkdown(txt) {
     } else {
       if (inBlock) {
         // Incoming content without a prefix is part of a block. Add to codeblock
-        codeBlock += `${lineData.content}\r`;
+        codeBlock += `${lineData.content}\n`;
       } else {
         // not in codeblock so regular paragraph text
         result.push(`<p>${lineData.content}</p>`);
@@ -55,10 +55,19 @@ function getLineData(line) {
   return result;
 }
 
-const markdownText =
-  "#Basic HTML\rThe key to writing basic HTML is basically just to learn a bunch of tags and then write them. A browser will render the text as a formatted web page.\rExample:\r```html\r<h1>This is a big heading</h1>\r<h2>This is a small heading</h2>\r<p>This is some normal paragraph text. It is common for paragraph text to be long but usually no more than (...wait for it...) a single paragraph</p>\r```\r##Headings\rYep, you'll need these, they are important. You'll find headings at the head of each section you write. Get it? Heading? Head? So awesome, glad we're all on the same page.\r##Parapgrah Text\rYep, you will need this too. Paragraph text may be boring but when you go to a website what would you guess people spend the most time looking at? If you guessed \"paragraph text\" you are *almost* right. It's actually videos of people and cats doing stupid stuff. Paragraph text is second longest.";
+let markdownArea = document.getElementById("markdown-input");
+markdownArea.defaultValue = "#Heading\n##Subheading\nNormal Text";
 
-let convertedText = convertMarkdown(markdownText);
-for (let element of convertedText) {
-  console.log(element);
+function convert() {
+  let markdownArea = document.getElementById("markdown-input");
+  let md = markdownArea.value;
+  let convertedHtmlArray = convertMarkdown(md);
+  let htmlDisplay = document.getElementById("html-display");
+  convertedHtmlArray.forEach((line) => {
+    let currentVal = htmlDisplay.innerHTML;
+    htmlDisplay.innerHTML = currentVal + line;
+  });
 }
+
+// const markdownText =
+//   "#Basic HTML\nThe key to writing basic HTML is basically just to learn a bunch of tags and then write them. A browser will render the text as a formatted web page.\nExample:\n```html\n<h1>This is a big heading</h1>\n<h2>This is a small heading</h2>\n<p>This is some normal paragraph text. It is common for paragraph text to be long but usually no more than (...wait for it...) a single paragraph</p>\n```\n##Headings\nYep, you'll need these, they are important. You'll find headings at the head of each section you write. Get it? Heading? Head? So awesome, glad we're all on the same page.\n##Parapgrah Text\nYep, you will need this too. Paragraph text may be boring but when you go to a website what would you guess people spend the most time looking at? If you guessed \"paragraph text\" you are *almost* right. It's actually videos of people and cats doing stupid stuff. Paragraph text is second longest.";
